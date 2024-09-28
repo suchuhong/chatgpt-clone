4a7931f0f9dcfabada99051e17459e7e4072a720
suchuhong <1442615457@qq.com>
2024-09-28 21:38:59 +0800

    React Rooter layout

--- src/main.jsx
+++ src/main.jsx
@@ -8,30 +8,41 @@ import DashboardPage from './routes/dashboardPage/DashboardPage'
 import ChatPage from './routes/chatPage/ChatPage'
 import SignInPage from './routes/signInPage/signInPage'
 import SignUpPage from './routes/signUpPage/signUpPage'
-
+import RootLayout from './layouts/rootLayout/RootLayout'
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout'
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Homepage />,
-  },
-  {
-    path: '/sign-in/*',
-    element: <SignInPage />,
-  },
-  {
-    path: '/sign-up/*',
-    element: <SignUpPage />,
-  },
-  {
-    path: '/dashboard',
+    element: <RootLayout />,
     children: [
       {
-        path: '/dashboard',
-        element: <DashboardPage />,
+        path: '/',
+        element: <Homepage />,
       },
       {
-        path: '/dashboard/chats/:id',
-        element: <ChatPage />,
+        path: '/sign-in/*',
+        element: <SignInPage />,
+      },
+      {
+        path: '/sign-up/*',
+        element: <SignUpPage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: '/dashboard',
+            children: [
+              {
+                path: '/dashboard',
+                element: <DashboardPage />,
+              },
+              {
+                path: '/dashboard/chats/:id',
+                element: <ChatPage />,
+              },
+            ],
+          },
+        ],
       },
     ],
   },
