febad17d94debccea2d87fcc7172c0b4c58c28ec
suchuhong <1442615457@qq.com>
2024-09-28 21:14:26 +0800

    1、引入react-router-dom 2、定义routers

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/homepage/Homepage'
+import DashboardPage from './routes/dashboardPage/DashboardPage'
+import ChatPage from './routes/chatPage/ChatPage'
+import SignInPage from './routes/signInPage/signInPage'
+import SignUpPage from './routes/signUpPage/signUpPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />,
+  },
+  {
+    path: '/sign-in/*',
+    element: <SignInPage />,
+  },
+  {
+    path: '/sign-up/*',
+    element: <SignUpPage />,
+  },
+  {
+    path: '/dashboard',
+    children: [
+      {
+        path: '/dashboard',
+        element: <DashboardPage />,
+      },
+      {
+        path: '/dashboard/chats/:id',
+        element: <ChatPage />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
