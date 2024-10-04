0b82995c7274bbd0e079850f7df70166911d7cc1
suchuhong <1442615457@qq.com>
2024-10-04 12:37:37 +0800

    对接gemini api

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -3,8 +3,12 @@ import './newPrompt.css'
 import { useEffect, useRef, useState } from 'react'
 import { IKImage } from 'imagekitio-react'
 import Upload from '../upload/Upload'
+import model from '../../lib/gemini'
+import Markdown from 'react-markdown'
 
 export default function NewPrompt() {
+  const [question, setQuestion] = useState('')
+  const [answer, setAnswer] = useState('')
   const [img, setImg] = useState({
     isLoading: false,
     error: '',
@@ -13,10 +17,33 @@ export default function NewPrompt() {
   })
 
   const endRef = useRef(null)
+  const formRef = useRef(null)
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: 'smooth' })
-  }, [])
+  }, [question, answer, img.dbData])
+
+  const add = async (text) => {
+    setQuestion(text)
+
+    try {
+      // const prompt = 'Write a story about a magic backpack.'
+      const result = await model.generateContent(text)
+      console.log(result.response.text())
+      setAnswer(result.response.text())
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const text = e.target.text.value
+    if (!text) return
+
+    add(text)
+  }
 
   return (
     <>
@@ -31,9 +58,17 @@ export default function NewPrompt() {
           transformation={[{ width: 380 }]}
         />
       )}
-      <div />
+      {/* 问题 */}
+      {question && <div className="message user">{question}</div>}
+      {/* 答案 */}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
+      {/* <button onClick={() => add()}>TEST AI</button> */}
       <div className="endChat" ref={endRef}></div>
-      <form className="newForm">
+      <form className="newForm" onSubmit={handleSubmit} ref={formRef}>
         <Upload setImg={setImg} />
         <input id="file" type="file" multiple={false} hidden />
         <input type="text" name="text" placeholder="Ask me anything..." />
